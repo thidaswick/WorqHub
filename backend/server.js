715e69caa53eb5f03a39b30b29ac5694796df48d
Thidas Wickramasinghe <thidaswickramasinghe8@gmail.com>
2026-03-24 15:02:43 +0530

    server.js change

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@
  */
 const app = require('./src/app');
 const { connectDB } = require('./src/config/db');
-const { port } = require('./src/config/env');
+
+const PORT = process.env.PORT || 5000;
 
 connectDB()
   .then(() => {
-    app.listen(port, () => console.log(`Server running on port ${port}`));
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   })
   .catch((err) => {
     console.error(err);
